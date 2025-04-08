a7dcf9a0116222a54640bad7e5796efc0e72fb44
Daniel-x2 <foconoaltar123@gmail.com>
2025-04-07 23:56:29 -0300

    estou ajustando o toplevel

--- recursos/login/toplevel/toplevel.py
+++ recursos/login/toplevel/toplevel.py
@@ -1,7 +1,36 @@
 import customtkinter
 import PIL
+import webbrowser 
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("600x500")
+        self.resizable(False,False)
+        self.title("help")
 
-class Toplevel(customtkinter.CTk):
-    def __init__(self, fg_color = None, **kwargs):
-        super().__init__(fg_color, **kwargs)
+        self.funçoes_init()
+    def funçoes_init(self):
+        self.fundo()
+        self.texto()
+    def fundo(self):    
+        #plano de fundo
+        plano_de_fundo=customtkinter.CTkLabel(self,bg_color="#242424",text="",width=600,height=500)
+        plano_de_fundo.place(x=0,y=0)
+    def texto(self):
+        text="""usuario: admin | senha: admin"""
+        label=customtkinter.CTkLabel(self,text=text,bg_color="#242424",text_color="white")
+        label.place(x=200,y=100)
+
+        agradecimentos="""   bem vindo ao meu projeto 
+        fico muito feliz que esteja olhando esse projeto
+        se estiver interessado nesse projeto ou em outros basta
+        acessar meu github"""
+        label_agra=customtkinter.CTkLabel(self,text=agradecimentos,font=("ariel",18),bg_color="#242424",text_color="white")
+        label_agra.place(x=40,y=250)
+    def abrir_navegador(self):
         
+
+
+
+app = App()
+app.mainloop()
