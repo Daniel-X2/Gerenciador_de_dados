797eb5e644a508c2949ac7d2d5b84745ec859154
Daniel-x2 <foconoaltar123@gmail.com>
2025-04-10 04:15:58 -0300

    removi as fonts

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import customtkinter
 import os
 from hashlib import sha256
-from recursos.fontes import fonts
 from recursos.login.login import  Tela_login
 from recursos.banco_de_dados.banco import Conexao
 
@@ -20,11 +19,7 @@ class App(customtkinter.CTk):
         #criaçao do frame
         frame_lateral=customtkinter.CTkFrame(self,fg_color="#242424",width=200,height=800,corner_radius=0)
         frame_lateral.grid()
-        #criaçao da label com fonte personalizada
-        fonte=fonts.fontes("CRUD")#esse e uma funçao que faz o necessario pra colocar a fonte, o unico parametro e do texto
-        image=customtkinter.CTkImage(fonte,size=(100,100))
-        label_font=customtkinter.CTkLabel(frame_lateral,text="",image=image)
-        label_font.place(x=50,y=10)
+        
 #aqui inicia a tela de login
 tela_login=Tela_login()
 tela_login.mainloop()
